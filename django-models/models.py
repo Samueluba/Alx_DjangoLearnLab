f53acb98fe5149bf8d171dd05264433d6178979a
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-05 16:59:16 +0100

    Update models.py

--- django-models/models.py
+++ django-models/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Author(models.Model):
-    """Represents an author who writes books."""
+    """Represents an author who can write many books."""
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -10,7 +10,7 @@ class Author(models.Model):
 
 
 class Book(models.Model):
-    """Each book belongs to one author (ForeignKey)."""
+    """A book written by one author (ForeignKey)."""
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
@@ -19,7 +19,7 @@ class Book(models.Model):
 
 
 class Library(models.Model):
-    """A library can have many books (ManyToMany)."""
+    """A library that contains many books (ManyToMany)."""
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
 
@@ -33,5 +33,5 @@ class Librarian(models.Model):
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
 
     def __str__(self):
-        return f"{self.name} (Librarian of {self.library.name})"
+        return f"{self.name} - Librarian of {self.library.name}"
 
