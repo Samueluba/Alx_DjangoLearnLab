9cdb11b91b8e2a5b94a5fa4f72d7491042764477
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-05 16:56:20 +0100

    Update models.py

--- django-models/models.py
+++ django-models/models.py
@@ -1,1 +1,37 @@
+from django.db import models
+
+
+class Author(models.Model):
+    """Represents an author who writes books."""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """Each book belongs to one author (ForeignKey)."""
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
+
+
+class Library(models.Model):
+    """A library can have many books (ManyToMany)."""
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    """Each library has exactly one librarian (OneToOne)."""
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return f"{self.name} (Librarian of {self.library.name})"
 
