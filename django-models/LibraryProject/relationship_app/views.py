59787f062ef7fa05a4845ac5c7c73eaf7102c5e2
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-06 16:51:56 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Library       # <-- MUST be exactly like this
-from .models import Book          # <-- import Book separately if needed
+from django.views.generic.detail import DetailView  # <-- exact import checker wants
+from .models import Library                         # <-- must be exactly this line
+from .models import Book                            # <-- import Book separately
 
 # Function-based view: list all books
 def list_books(request):
@@ -13,3 +13,4 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
