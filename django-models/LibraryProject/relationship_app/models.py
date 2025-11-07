6e10fd91b0b9261f9a9a9f8efe6f5c5ffb230174
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-07 16:49:17 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -27,3 +27,19 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a new book"),
+            ("can_change_book", "Can edit existing books"),
+            ("can_delete_book", "Can delete books"),
+        ]
