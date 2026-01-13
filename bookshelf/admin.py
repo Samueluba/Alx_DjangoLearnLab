3163ab25f1f90936d69f288911235a046b6d5b55
samueluba <samueluba001@gmail.com>
2026-01-13 15:58:17 +0100

    Add custom user model with manager and admin integration

---
+++ bookshelf/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Extra Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    list_display = ('username', 'email', 'first_name',
+                    'last_name', 'is_staff', 'date_of_birth')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# Register your models here.
