e2e33677c949f8bf49b76bae47d396f1482b4402
samueluba <samueluba001@gmail.com>
2026-01-13 16:08:03 +0100

    Move bookshelf app inside LibraryProject folder for checker

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',  # <-- add this
+    ''LibraryProject.bookshelf',  # <-- add this
 ]
 
 
