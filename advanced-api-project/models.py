9b9763c86b14390e24dfb641f56a9d6c2210aed9
Samueluba <111093750+Samueluba@users.noreply.github.com>
2026-01-13 13:01:12 +0100

    Create models.py

---
+++ advanced-api-project/models.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from django.db import models
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
